458d4b7e6944fbf666e970df9bf06992e432f35e
AntonHuggard <56659693+AntonHuggard@users.noreply.github.com>
2020-05-11 12:10:39 +1200

    Add files via upload

--- dynamic_content.js
+++ dynamic_content.js
@@ -3,11 +3,57 @@ const current_cost_for_new_members = 100;
 const current_ongoing_cost = 80;
 
 function include_location() {
-    document.getElementById("about_meeting_location").innerHTML = current_meeting_location;
+    document.getElementsByClassName("about_meeting_location").innerHTML = current_meeting_location;
     document.getElementById("ongoing_cost").innerHTML = current_ongoing_cost;
     document.getElementById("cost_for_new_members").innerHTML = current_cost_for_new_members;
 }
 
+function change_dir(path) {
+    const header =
+        `
+        <div id = "mobile_nav">
+            <img onclick='show_menu()' class='menu_btn' src ='${path}resources/hamburger.png'>
+            <h1>V A R S I T Y</h1>
+        </div>
+        <div id = "dropdown_menu" class = "dropdown_content">
+            <a href='${path}index.html'>HOME</a>
+            <a href='${path}about.html'>ABOUT</a>
+            <a href='${path}contact.html'>CONTACT</a>
+            <a href='${path}membership.html'>MEMBERSHIP</a>
+            <a href='${path}rsrcs.html'>RESOURCES</a>
+            <a href='${path}other.html'>OTHER</a>
+        </div>
+        <div class = "top">
+            <img src = "${path}resources/tm-logo.jpg">
+            <h1>VARSITY TOASTMASTERS</h1>
+            <h2>Est. 1999</h2>
+        </div>
+        <div id = "desktop_nav">
+            <a href='${path}index.html'>Home</a>
+            <a href='${path}about.html'>About</a>
+            <a href='${path}contact.html'>Contact</a>
+            <a href='${path}membership.html'>Membership</a>
+            <a href='${path}rsrcs.html' id = 'rescources_thing'>Resources</a>
+            <a href='${path}other.html'>Other</a>
+        </div>
+        `;
+    document.getElementsByTagName('header')[0].innerHTML = header;
+
+    let curr_year = new Date().getFullYear();
+    const footer =
+        `
+        <div id = "pick_your_pipe">
+            <!-- Icons from Freepik Content (https://www.flaticon.com/) -->
+            <img src = "${path}resources/fb-icon-00114a.svg" onclick="window.location.href= 'https\:\/\/www.facebook.com/varsitytoastmastersuoa/' ">
+            <img src = "${path}resources/ig-icon-dark.svg" onclick="window.location.href= 'https\:\/\/www.instagram.com/varsitytoastmastersnz/' ">
+            <img src = "${path}resources/yt-icon-light.svg" onclick="window.location.href=  'https\:\/\/www.youtube.com/watch?v=dQw4w9WgXcQ' ">
+        </div>
+        <p>&copy Varsity Toastmasters ${curr_year}</p>
+        `;
+    document.getElementsByTagName('footer')[0].innerHTML = footer; // putting the content inside the footer tag
+
+
+}
 
 const header =
 `
@@ -54,14 +100,14 @@ const sider =
 document.getElementById('side').innerHTML = sider;
 
 let curr_year = new Date().getFullYear();
-const footer =
-`
-<div id = "pick_your_pipe">
-    <!-- Icons from Freepik Content (https://www.flaticon.com/) -->
-    <img src = "resources/fb-icon-00114a.svg" onclick="window.location.href= 'https\:\/\/www.facebook.com/varsitytoastmastersuoa/' ">
-    <img src = "resources/ig-icon-dark.svg" onclick="window.location.href= 'https\:\/\/www.instagram.com/varsitytoastmastersnz/' ">
-    <img src = "resources/yt-icon-light.svg" onclick="window.location.href=  'https\:\/\/www.youtube.com/watch?v=dQw4w9WgXcQ' ">
-</div>
-<p>&copy Varsity Toastmasters ${curr_year}</p>
-`;
-document.getElementsByTagName('footer')[0].innerHTML = footer;
+    const footer =
+        `
+        <div id = "pick_your_pipe">
+            <!-- Icons from Freepik Content (https://www.flaticon.com/) -->
+            <img src = "resources/fb-icon-00114a.svg" onclick="window.location.href= 'https\:\/\/www.facebook.com/varsitytoastmastersuoa/' ">
+            <img src = "resources/ig-icon-dark.svg" onclick="window.location.href= 'https\:\/\/www.instagram.com/varsitytoastmastersnz/' ">
+            <img src = "resources/yt-icon-light.svg" onclick="window.location.href=  'https\:\/\/www.youtube.com/watch?v=dQw4w9WgXcQ' ">
+        </div>
+        <p>&copy Varsity Toastmasters ${curr_year}</p>
+        `;
+    document.getElementsByTagName('footer')[0].innerHTML = footer; // putting the content inside the footer tag
